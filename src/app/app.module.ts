55ce25000aae52ed682c4c94fa73a50da989b62c
Francisj14 <75384294+Francisj14@users.noreply.github.com>
2023-12-15 21:45:56 -0500

    actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,9 +17,15 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+    apiKey: "AIzaSyCWjwkIlUCpDiu-lbLHpfozttUiU8638rY",
+    authDomain: "admin-c9773.firebaseapp.com",
+    databaseURL: "https://admin-c9773-default-rtdb.firebaseio.com",
+    projectId: "admin-c9773",
+    storageBucket: "admin-c9773.appspot.com",
+    messagingSenderId: "545000259753",
+    appId: "1:545000259753:web:8f1df91aff5c118c11a9b8"
+    }
+  };
